dc99a42d45c3140e62c90a32a6dbc164231d0088
Alex Bondarenko <albom2004q@gmail.com>
2023-07-16 14:36:25 +0300

    Adding Areas to the project

--- MightyMats/Areas/Customer/Controllers/HomeController.cs
+++ MightyMats/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using MightyMatsData.Models;
 
 namespace MightyMats.Controllers;
 
+[Area("Customer")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;

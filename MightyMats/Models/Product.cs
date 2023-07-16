446b32e2b37f775616d0d35f6a54806ac72555f4
Alex Bondarenko <albom2004q@gmail.com>
2023-07-16 13:27:21 +0300

    Adding Product model to the project

---
+++ MightyMats/Models/Product.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MightyMats.Models;
+
+public class Product
+{
+    [Key] public int Id { get; set; }
+    
+    [Required] public string Title { get; set; }
+    
+    [MaxLength(299, ErrorMessage = "Description must contain less than 300 symbols")]
+    public string? Description { get; set; }
+    
+    [Required] public decimal Price { get; set; }
+    
+    [MaxLength(399, ErrorMessage = "Details field must contain less than 400 symbols")]
+    public string? Details { get; set; }
+}

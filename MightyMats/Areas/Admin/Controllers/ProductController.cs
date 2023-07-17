a9f78243326b9c7925e00e0ce679d08ebe4078b5
Alex Bondarenko <albom2004q@gmail.com>
2023-07-17 13:41:50 +0300

    Create Action method has been created successfuly

--- MightyMats/Areas/Admin/Controllers/ProductController.cs
+++ MightyMats/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MightyMatsData.Models;
 using MightyMatsData.UnitOfWork;
 
 namespace MightyMats.Controllers;
@@ -7,21 +8,34 @@ namespace MightyMats.Controllers;
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
-    
+
     public ProductController(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
     }
-    
-    
+
+
     public IActionResult Index()
     {
         var productsList = _unitOfWork._productRepository.GetAll(null).Result;
         return View(productsList);
     }
-    
+
     public IActionResult Create()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork._productRepository.Add(product);
+            _unitOfWork._productRepository.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
         return View();
     }
 }

0368ab9d488e27e6e2a57103f0e4d630959440a6
Alex Bondarenko <albom2004q@gmail.com>
2023-07-20 14:03:00 +0300

    Adding StaticDetails

---
+++ MightyMatsData/StaticDetails.cs
@@ -0,0 +1,19 @@
+namespace MightyMatsData;
+
+public class StaticDetails
+{
+    public const string AdminRole = "Admin";
+    public const string CustomerRole = "Customer";
+    
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusInProcess = "Processing";
+    public const string StatusShipped = "Shipped";
+    public const string StatusCancelled = "Cancelled";
+    public const string StatusRefunded = "Refunded";
+
+    public const string PaymentStatusPending = "Pending";
+    public const string PaymentStatusApproved = "Approved";
+    public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+    public const string PaymentStatusRejected = "Rejected";
+}

b612a29f5a6b01abd6cfc1a07101e15b9120a7cf
Alex Bondarenko <albom2004q@gmail.com>
2023-07-21 20:02:18 +0300

    Adding SessionId row to Db

--- MightyMatsData/Models/OrderHeader.cs
+++ MightyMatsData/Models/OrderHeader.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -10,9 +11,7 @@ public class OrderHeader
     public int Id { get; set; }
     public string UserId { get; set; }
 
-    [ForeignKey("UserId")]
-    [ValidateNever]
-    public IdentityUser User { get; set; }
+    [ForeignKey("UserId")] [ValidateNever] public IdentityUser User { get; set; }
 
     public DateTime OrderDate { get; set; }
     public DateTime ShippingDate { get; set; }
@@ -26,12 +25,13 @@ public class OrderHeader
     public DateTime PaymentDate { get; set; }
     public DateTime PaymentDueDate { get; set; }
 
+    public string? SessionId { get; set; }
     public string? PaymentIntentId { get; set; }
 
-    [Required] public string PhoneNumber { get; set; }
-    [Required] public string StreetAddress { get; set; }
+    [Required, DisplayName("Phone")] public string PhoneNumber { get; set; }
+    [Required, DisplayName("Street Address")] public string StreetAddress { get; set; }
     [Required] public string City { get; set; }
     [Required] public string State { get; set; }
-    [Required] public string PostalCode { get; set; }
+    [Required, DisplayName("Postal Code")] public string PostalCode { get; set; }
     [Required] public string Name { get; set; }
 }

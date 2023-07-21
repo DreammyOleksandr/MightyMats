9586fc5c93c28bc1c9504a0ceaba83069e5ff4ce
Alex Bondarenko <albom2004q@gmail.com>
2023-07-21 20:02:31 +0300

    Adding helper stripe methods

--- MightyMatsData/Repository/IRepository/IOrderHeaderRepository.cs
+++ MightyMatsData/Repository/IRepository/IOrderHeaderRepository.cs
@@ -4,5 +4,6 @@ namespace MightyMatsData.Repository.IRepository;
 
 public interface IOrderHeaderRepository : IRepository<OrderHeader>
 {
-    
+    void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+    void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId);
 }
--- MightyMatsData/Repository/OrderHeaderRepository.cs
+++ MightyMatsData/Repository/OrderHeaderRepository.cs
@@ -5,7 +5,36 @@ namespace MightyMatsData.Repository;
 
 public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
 {
+    private readonly ApplicationDbContext _db;
+
     public OrderHeaderRepository(ApplicationDbContext db) : base(db)
     {
+        _db = db;
+    }
+
+    public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+    {
+        var orderFromDb = _db.OrderHeaders.FirstOrDefault(_ => _.Id == id);
+        if (orderFromDb != null)
+        {
+            orderFromDb.OrderStatus = orderStatus;
+            if (!string.IsNullOrEmpty(paymentStatus))
+                orderFromDb.PaymentStatus = paymentStatus;
+        }
+    }
+
+    public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+    {
+        var orderFromDb = _db.OrderHeaders.FirstOrDefault(_ => _.Id == id);
+        if (!string.IsNullOrEmpty(sessionId))
+        {
+            orderFromDb.SessionId = sessionId;
+        }
+
+        if (!string.IsNullOrEmpty(paymentIntentId))
+        {
+            orderFromDb.PaymentIntentId = paymentIntentId;
+            orderFromDb.OrderDate = DateTime.Now;
+        }
     }
 }

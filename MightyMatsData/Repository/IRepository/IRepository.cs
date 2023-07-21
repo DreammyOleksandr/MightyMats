c743401f7c177597bba3bc82dc944ef1103f296c
Alex Bondarenko <albom2004q@gmail.com>
2023-07-21 21:28:58 +0300

    finishing with Stripe

--- MightyMatsData/Repository/IRepository/IRepository.cs
+++ MightyMatsData/Repository/IRepository/IRepository.cs
@@ -5,7 +5,7 @@ namespace MightyMatsData.Repository.IRepository;
 public interface IRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter = null, string? includeProps = null);
-    Task<T> Get(Expression<Func<T, bool>> filter);
+    Task<T> Get(Expression<Func<T, bool>> filter, string? includeProps = null);
     Task Add(T entity);
     void Update(T entity);
     void Remove(T entity);

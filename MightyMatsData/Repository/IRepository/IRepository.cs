5be88cba904ecd23d7eb5f07aa911cc7faf6669e
Alex Bondarenko <albom2004q@gmail.com>
2023-07-23 10:22:12 +0300

    Creating Order MVC

--- MightyMatsData/Repository/IRepository/IRepository.cs
+++ MightyMatsData/Repository/IRepository/IRepository.cs
@@ -4,7 +4,7 @@ namespace MightyMatsData.Repository.IRepository;
 
 public interface IRepository<T> where T : class
 {
-    Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter = null, string? includeProps = null);
+    Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProps = null);
     Task<T> Get(Expression<Func<T, bool>> filter, string? includeProps = null);
     Task Add(T entity);
     void Update(T entity);

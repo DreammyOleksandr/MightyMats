1e24125e9cb18e7b79683ee11e1a6b14e7b03cc7
Alex Bondarenko <albom2004q@gmail.com>
2023-07-28 16:55:37 +0300

    Add to cart from main menu functionality

--- MightyMats/Areas/Customer/Controllers/HomeController.cs
+++ MightyMats/Areas/Customer/Controllers/HomeController.cs
@@ -4,25 +4,63 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MightyMatsData.Models;
 using MightyMatsData.UnitOfWork;
+using NToastNotify;
 
 namespace MightyMats.Controllers;
 
 [Area("Customer")]
 public sealed class HomeController : Controller
 {
+    private readonly IToastNotification _toastNotification;
     private readonly ILogger<HomeController> _logger;
     private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, IToastNotification toastNotification)
     {
         _logger = logger;
         _unitOfWork = unitOfWork;
+        _toastNotification = toastNotification;
     }
 
     public async Task<IActionResult> Index()
     {
         var products = await _unitOfWork._productRepository.GetAll();
         return View(products);
+    }
+
+    [HttpPost, Authorize]
+    public async Task<IActionResult> IndexPOST(int productId)
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        ShoppingCartItem cart = new()
+        {
+            Product = _unitOfWork._productRepository.Get(_ => _.Id == productId).Result,
+            ProductId = productId,
+            UserId = userId,
+            Count = 1,
+        };
+
+        var shoppingCartItemFromDb =
+            await _unitOfWork._shoppingCartItemRepository.Get(_ =>
+                _.UserId == userId && _.ProductId == cart.ProductId);
+
+        if (!ReferenceEquals(shoppingCartItemFromDb, null))
+        {
+            shoppingCartItemFromDb.Count += cart.Count;
+            _unitOfWork._shoppingCartItemRepository.Update(shoppingCartItemFromDb);
+            _toastNotification.AddSuccessToastMessage("Mat was successfully added to the cart!");
+        }
+        else
+        {
+            await _unitOfWork._shoppingCartItemRepository.Add(cart);
+            _toastNotification.AddSuccessToastMessage("Mat was successfully added to the cart!");
+        }
+
+        await _unitOfWork._shoppingCartItemRepository.Save();
+
+        return RedirectToAction(nameof(Index));
     }
 
     public IActionResult Privacy()

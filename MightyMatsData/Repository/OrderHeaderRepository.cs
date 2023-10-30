1578f5e8e1c6e18d80b02458eb9bdc48dc5cc695
Bondarenko Oleksandr <albom2004q@gmail.com>
2023-10-30 15:24:20 +0200

    Order Repository

--- MightyMatsData/Repository/OrderHeaderRepository.cs
+++ MightyMatsData/Repository/OrderHeaderRepository.cs
@@ -1,39 +1,38 @@
-// using MightyMatsData.Models;
-// using MightyMatsData.Repository.IRepository;
-//
-// namespace MightyMatsData.Repository;
-//
-// public sealed class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
-// {
-//     private readonly ApplicationDbContext _db;
-//
-//     public OrderHeaderRepository(ApplicationDbContext db) : base(db)
-//     {
-//         _db = db;
-//     }
-//     //
-//     // public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
-//     // {
-//     //     var orderFromDb = _db.OrderHeaders.FirstOrDefault(_ => _.Id == id);
-//     //     if (!ReferenceEquals(orderFromDb, null))
-//     //     {
-//     //         orderFromDb.OrderStatus = orderStatus;
-//     //         if (!string.IsNullOrEmpty(paymentStatus))
-//     //             orderFromDb.PaymentStatus = paymentStatus;
-//     //     }
-//     // }
-//     //
-//     // public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
-//     // {
-//     //     var orderFromDb = _db.OrderHeaders.FirstOrDefault(_ => _.Id == id);
-//     //     if (!string.IsNullOrEmpty(sessionId)) orderFromDb.SessionId = sessionId;
-//     //
-//     //     if (!string.IsNullOrEmpty(paymentIntentId))
-//     //     {
-//     //         orderFromDb.PaymentIntentId = paymentIntentId;
-//     //         orderFromDb.OrderDate = DateTime.Now;
-//     //         orderFromDb.PaymentDate = DateTime.Now;
-//     //     }
-//     // }
-// }
+using MightyMatsData.Models;
+using MightyMatsData.Repository.IRepository;
 
+namespace MightyMatsData.Repository;
+
+public sealed class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+    {
+        var orderFromDb = _db.OrderHeaders.FirstOrDefault(_ => _.Id == id);
+        if (!ReferenceEquals(orderFromDb, null))
+        {
+            orderFromDb.OrderStatus = orderStatus;
+            if (!string.IsNullOrEmpty(paymentStatus))
+                orderFromDb.PaymentStatus = paymentStatus;
+        }
+    }
+
+    public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+    {
+        var orderFromDb = _db.OrderHeaders.FirstOrDefault(_ => _.Id == id);
+        if (!string.IsNullOrEmpty(sessionId)) orderFromDb.SessionId = sessionId;
+
+        if (!string.IsNullOrEmpty(paymentIntentId))
+        {
+            orderFromDb.PaymentIntentId = paymentIntentId;
+            orderFromDb.OrderDate = DateTime.Now;
+            orderFromDb.PaymentDate = DateTime.Now;
+        }
+    }
+}
